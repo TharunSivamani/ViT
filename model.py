76efc2ad5fe68b0ba8582d312c0f6af6ec7e2a2a
Tharun <tharunsivamani@gmail.com>
2023-12-08 20:15:49 +0530

    ViT Block

--- model.py
+++ model.py
@@ -99,6 +99,43 @@ class TransformerEncoderBlock(nn.Module):
 
         return x
 
+class ViT(nn.Module):
+    """
+    Creates a ViT Model
+    """
+    def __init__(self, img_size=224, in_channels=3, patch_size=16, num_transformer_layer=12, embedding_dim=768, mlp_size=3072, num_heads=12, attn_dropout=0, ml_dropout=0.1, embedding_dropout=0.1, num_classes=1000):
+        super().__init__()
+
+        assert img_size % patch_size == 0, f"Image Size must be divisible by patch size, given image shape: {img_size}, patch_size: {patch_size}"
+        # (height * width/patch^2)
+        self.num_patches = (img_size * img_size) // patch_size ** 2
+        self.class_embedding = nn.Parameter(data = torch.randn(1, 1, embedding_dim), requires_grad=True)
+        self.position_embedding = nn.Parameter(data = torch.randn(1, self.num_patches+1, embedding_dim), requires_grad = True)
+        self.embedding_dropout = nn.Dropout(p=embedding_dropout)
+        self.patch_embedding = PatchEmbedding(in_channels=in_channels, patch_size=patch_size, embedding_dim=embedding_dim)
+
+        self.transformer_encoder = nn.Sequential(
+            *[TransformerEncoderBlock(embedding_dim=embedding_dim, num_heads=num_heads, mlp_size=mlp_size, mlp_dropout=ml_dropout) for _ in range(num_transformer_layer)]
+        )
+
+        self.classifier = nn.Sequential(
+            nn.LayerNorm(normalized_shape=embedding_dim),
+            nn.Linear(in_features=embedding_dim, out_features=num_classes)
+        )
+    
+    def forward(self, x):
+
+        batch_size = x.shape[0]
+        class_token = self.class_embedding.expand(batch_size, -1, -1)
+        x = self.patch_embedding(x)
+        x = torch.cat([class_token, x], dim=1)
+        x = self.position_embedding(x) + x
+        x = self.embedding_dropout(x)
+        x = self.transformer_encoder(x)
+        x = self.classifier(x[:, 0])
+
+        return x
+
 if __name__ == "__main__":
 
     patch = PatchEmbedding(3, 16, 768)
